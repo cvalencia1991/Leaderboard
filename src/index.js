ee029eb0e2cee3e9ed9e92142fe1d20cac12cc6a
cvalencia1991 <cesar4a6z@gmail.com>
2022-10-12 22:26:11 -0500

    Edit Styles.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import Scorelist from './modules/scorelist.js';
 import refreshpage from './modules/refresh.js';
+import getinformation from './modules/sendinformation.js';
 
 const submit = document.getElementById('submit');
 const refresh = document.getElementById('refresh');
@@ -11,25 +12,9 @@ submit.addEventListener('click', (e) => {
   e.preventDefault();
   const name = document.getElementById('nameplayer').value;
   const scoreplayer = document.getElementById('scoreplayer').value;
-  const getResponse2 = async () => {
-    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/3Gp8KLZKuYejD2oYH5ly/scores';
-    const response = await fetch(url,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          user: name,
-          score: scoreplayer,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
-    const data = await response.json();
-    return data;
-  };
   if (name !== '' || scoreplayer !== '') {
     const scorelistitem = new Scorelist(name, scoreplayer);
     scorelistitem.addscorelist();
-    getResponse2();
+    getinformation(name, scoreplayer);
   }
 });
