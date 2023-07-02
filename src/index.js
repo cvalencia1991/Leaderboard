a5c3230fd8f7441f669caf93418149f966933848
Cesar Valencia <cesar4a6z@gmail.com>
2023-07-01 21:33:21 -0500

    Fix Bug

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import './style.css';
-import Scorelist from './modules/scorelist.js';
-import refreshpage from './modules/refresh.js';
-import getinformation from './modules/sendinformation.js';
+import "./style.css";
+import Scorelist from "./modules/scorelist.js";
+import { refreshPage, getInfo } from "./modules/endpoints.js";
 
-const submit = document.getElementById('submit');
-const refresh = document.getElementById('refresh');
+const submit = document.getElementById("submit");
+const refresh = document.getElementById("refresh");
+const scoreListUl = document.getElementById("scoreslist");
 
-refresh.addEventListener('click', refreshpage);
-
-submit.addEventListener('click', (e) => {
+refresh.addEventListener("click", async (e) => {
   e.preventDefault();
-  const name = document.getElementById('nameplayer').value;
-  const scoreplayer = document.getElementById('scoreplayer').value;
-  if (name !== '' || scoreplayer !== '') {
+  const data = await refreshPage();
+  scoreListUl.innerHTML = "";
+  data.result.forEach((score) => {
+    const scorelist = new Scorelist(score.user, score.score);
+    scorelist.addscorelist();
+  });
+});
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("nameplayer").value;
+  const scoreplayer = document.getElementById("scoreplayer").value;
+  if (name !== "" || scoreplayer !== "") {
     const scorelistitem = new Scorelist(name, scoreplayer);
     scorelistitem.addscorelist();
-    getinformation(name, scoreplayer);
+    getInfo(name, scoreplayer);
   }
 });
