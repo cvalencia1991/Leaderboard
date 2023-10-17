093c4a657d76f69e7535b1b21c45c6b9441b291e
Cesar Valencia <cesar4a6z@gmail.com>
2023-10-16 22:28:18 -0500

    Implement Favico in Webpack

--- src/modules/endpoints.js
+++ src/modules/endpoints.js
@@ -1,60 +1,57 @@
-const URL_POST =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/vr0F3gIgcu44WrsSv7t4/scores";
-const URL_GET =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/vr0F3gIgcu44WrsSv7t4/scores";
-  const URL_GETID =
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
+const URL_POST = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/vr0F3gIgcu44WrsSv7t4/scores';
+const URL_GET = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/vr0F3gIgcu44WrsSv7t4/scores';
+const URL_GETID = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
-export const refreshPage =  async () => {
+export const refreshPage = async () => {
   const response = await fetch(URL_GET, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
+  let data;
   try {
     if (response.ok) {
-      const data = await response.json();
-      return data;
+      data = await response.json();
     }
   } catch (err) {
     throw new Error(err);
   }
-}
+  return data;
+};
 
 export const getInfo = async (name, scoreplayer) => {
   const response = await fetch(URL_POST, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       user: name,
       score: scoreplayer,
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
   const data = await response.json();
   return data;
-}
+};
 
-export const getid = async() => {
+export const getid = async () => {
   const response = await fetch(URL_GETID, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
-      name: "My brand new game cool game",
+      name: 'My brand new game cool game',
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
+  let data;
   try {
     if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      return data;
+      data = await response.json();
     }
   } catch (err) {
     throw new Error(err);
   }
-}
-
+  return data;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      favicon: './src/images/favicon.ico',
     }),
   ],
   module: {
