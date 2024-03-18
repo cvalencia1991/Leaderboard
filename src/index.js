09b63aefbb94e4653a88f3b01721040144ef0ffd
cvalencia1991 <cesar4a6z@gmail.com>
2024-03-18 16:05:15 -0500

    Change the way you submit and show the information
    - reduce the lines of code to send the score and the user
    - Implement two new functions to create and refresh the leader board
      screen

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import './style.css';
-import Scorelist from './modules/scorelist.js';
 import refreshPage from './modules/refreshPage.js';
-import getInfo from './modules/getinfo.js';
+import createPlayer from './modules/createPlayer.js';
 
 const submit = document.getElementById('submit');
 const refresh = document.getElementById('refresh');
-const scoreListUl = document.getElementById('scoreslist');
 
-refresh.addEventListener('click', async (e) => {
-  e.preventDefault();
-  const data = await refreshPage();
-  scoreListUl.innerHTML = '';
-  data.result.forEach((score) => {
-    const scorelist = new Scorelist(score.user, score.score);
-    scorelist.addscorelist();
-  });
-});
+refresh.addEventListener('click', refreshPage);
 
 submit.addEventListener('click', (e) => {
   e.preventDefault();
   const name = document.getElementById('nameplayer').value;
   const scoreplayer = document.getElementById('scoreplayer').value;
-  if (name !== '' || scoreplayer !== '') {
-    const scorelistitem = new Scorelist(name, scoreplayer);
-    scorelistitem.addscorelist();
-    getInfo(name, scoreplayer);
-  }
+  createPlayer(name, scoreplayer);
 });
